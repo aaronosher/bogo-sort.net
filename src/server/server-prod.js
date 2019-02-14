5420a3dfc721393fb2e19b6e1d01baaa17618555
Aaron Osher <aaron@aaronosher.io>
2019-02-14 12:06:13 +0000

    Production Build

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -2,7 +2,8 @@ import path from 'path';
 import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
-import './sorts';
+import SocketHandler from './sockets';
+import sorts from './sorts';
 
 const app = express(),
             DIST_DIR = __dirname,
@@ -11,20 +12,14 @@ app.use(express.static(DIST_DIR))
 app.get('*', (req, res) => {
     res.sendFile(HTML_FILE)
 })
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3030
 
 const server = http.Server(app);
 
+const socketHandler = new SocketHandler(server);
+
 server.listen(PORT, () => {
-    console.log(`App listening to ${PORT}....`)
-    console.log('Press Ctrl+C to quit.')
-})
-
-const io = SocketIO(server);
-
-io.on('connection', (socket) => {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-  });
+    console.info(`App listening to ${PORT}....`)
+    console.info('Press Ctrl+C to quit.')
+    sorts(data => socketHandler.sendToAll('sort', data));
+});
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
   entry: {
@@ -18,10 +18,11 @@ module.exports = {
   // Webpack 5 will likely come with one
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
+      new TerserPlugin({
         parallel: true,
-        sourceMap: true // set to true if you want JS source maps
+        terserOptions: {
+          ecma: 6,
+        },
       }),
       new OptimizeCSSAssetsPlugin({})
     ]
